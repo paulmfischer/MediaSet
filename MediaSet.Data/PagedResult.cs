c34e649230d9faf79a7fc4698e3beb34cd8756f2
Paul Fischer <paul.fischer@ilmservice.com>
2020-01-17 22:43:44 -0600

    add missed new file

---
+++ MediaSet.Data/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MediaSet.Data
+{
+    public class PagedResult<T>
+    {
+        public IList<T> Items { get; set; }
+        public int Total { get; set; }
+    }
+}
