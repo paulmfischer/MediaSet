89db01de37789de9f1eeee9c5b3c97b00ee9506b
Paul Fischer <me@paulmfischer.dev>
2023-10-27 17:07:10 -0500

    adding missed migrations

---
+++ MediaSet.API/Migrations/20231027220621_UpdateBookData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaSet.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Books",
+                newName: "Title");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfPages",
+                table: "Books",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Plot",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicationYear",
+                table: "Books",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfPages",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Plot",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "PublicationYear",
+                table: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Books",
+                newName: "Name");
+        }
+    }
+}
