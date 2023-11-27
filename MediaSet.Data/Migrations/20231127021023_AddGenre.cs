734bb0dfbb3a3034b9287a01137f7fc5e6040fb6
Paul Fischer <me@paulmfischer.dev>
2023-11-26 20:30:44 -0600

    forgot migration

---
+++ MediaSet.Data/Migrations/20231127021023_AddGenre.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaSet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Books",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_GenreId",
+                table: "Books",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Genres_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_GenreId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Books");
+        }
+    }
+}
