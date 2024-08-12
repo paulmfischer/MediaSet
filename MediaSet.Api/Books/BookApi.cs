ef453ccb68e92017b8f81aeac880f85e986fd391
Paul Fischer <me@paulmfischer.dev>
2024-08-12 16:53:16 -0500

    disable submit btn on add/edit, wire up edit

--- MediaSet.Api/Books/BookApi.cs
+++ MediaSet.Api/Books/BookApi.cs
@@ -14,12 +14,8 @@ internal static class BookApi
 
     group.WithTags("Books");
 
-    group.MapGet("/", async (BooksService booksService) => await booksService.GetAsync()).WithOpenApi();
-    group.MapGet("/search", async (BooksService bookService, string searchText, string orderBy) => {
-      var books = await bookService.SearchAsync(searchText, orderBy);
-      Console.WriteLine("Searching for books by {0} and ordering by {1}, first book title: {2}", searchText, orderBy, books.ElementAt(0).Title);
-      return books;
-    });
+    group.MapGet("/", async (BooksService booksService) => await booksService.GetAsync());
+    group.MapGet("/search", async (BooksService bookService, string searchText, string orderBy) => await bookService.SearchAsync(searchText, orderBy));
 
     group.MapGet("/{id}", async Task<Results<Ok<Book>, NotFound>> (BooksService booksService, string id) =>
     {
@@ -28,7 +24,7 @@ internal static class BookApi
         Book book => TypedResults.Ok(book),
         _ => TypedResults.NotFound()
       };
-    }).WithOpenApi();
+    });
 
     group.MapPost("/", async Task<Results<Created<Book>, BadRequest>> (BooksService booksService, Book newBook) =>
     {
@@ -40,7 +36,7 @@ internal static class BookApi
       await booksService.CreateAsync(newBook);
 
       return TypedResults.Created($"/books/{newBook.Id}", newBook);
-    }).WithOpenApi();
+    });
 
     group.MapPut("/{id}", async Task<Results<Ok, NotFound, BadRequest>> (BooksService booksService, string id, Book updatedBook) =>
     {
