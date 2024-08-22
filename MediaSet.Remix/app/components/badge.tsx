b67c23add1a66b48d413b147efb7c7ea2ba3a233
Paul Fischer <me@paulmfischer.dev>
2024-08-22 13:32:56 -0500

    ability to add/remove from multi-select

--- MediaSet.Remix/app/components/badge.tsx
+++ MediaSet.Remix/app/components/badge.tsx
@@ -6,7 +6,8 @@ type BadgeProps = {
 
 export default function Badge({ children }: PropsWithChildren) {
   return (
-    <span className="inline-flex items-center rounded-md bg-zinc-700 px-2 py-1 text-xs font-medium text-slate-300 ring-1 ring-inset ring-slate-400">
+    // <span className="inline-flex items-center rounded-md bg-zinc-700 px-2 py-1 text-xs font-medium text-slate-300 ring-1 ring-inset ring-slate-400">
+    <span className="inline-flex items-center rounded-md bg-slate-300 px-2 py-1 text-xs font-medium text-zinc-700 ring-1 ring-inset ring-slate-400">
       {children}
     </span>
   )
--- MediaSet.Remix/app/components/multiselect-input.tsx
+++ MediaSet.Remix/app/components/multiselect-input.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Badge from "./badge";
+import { IconX } from "@tabler/icons-react";
 
 type Option = {
   label: string;
@@ -12,10 +13,17 @@ type MultiselectProps = {
   options: Option[];
 };
 
+let count = 0;
 export default function MultiselectInput(props: MultiselectProps) {
+  count++;
   const [selected, setSelected] = useState<string[]>([]);
   const [filtered, setFiltered] = useState<Option[]>(props.options);
   const [displayOptions, setDisplayOptions] = useState(false);
+  const [inputEl, setInputEl] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setInputEl(document.getElementById(`multi-select-input-${count}`));
+  });
 
   const isSelected = (option: Option) => selected.includes(option.value);
   const toggleSelected = (option: Option) => {
@@ -34,16 +42,22 @@ export default function MultiselectInput(props: MultiselectProps) {
   };
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex gap-2">
-        {selected.map(selected => (<Badge>{selected}</Badge>))}
+    <>
+      <div className={`absolute z-40 w-full h-full ${displayOptions ? '' : 'hidden'}`} onClick={() => setDisplayOptions(false)}></div>
+      <div className="flex flex-col gap-2 z-50">
+        <div className="flex gap-2 bg-white p-1 rounded-sm" id={`multi-select-input-${count}`}>
+          {selected.map(selected => (<Badge><div className="flex gap-2" onClick={() => toggleSelected({ label: selected, value: selected })}>{selected}<IconX size={16} /></div></Badge>))}
+          <input type="text" className="flex-1 pl-1 p-0 outline-none" onFocus={() => setDisplayOptions(true)} onChange={(event) => filterResults(event.target.value)} />
+        </div>
+        <div 
+          className={`fixed py-2 rounded-md max-h-80 min-w-80 overflow-scroll dark:bg-zinc-700 ${displayOptions ? '' : 'hidden'}`}
+          style={{ top: (inputEl?.offsetTop ?? 0) + (inputEl?.offsetHeight ?? 0) + 5, left: inputEl?.offsetLeft, width: inputEl?.offsetWidth }}
+        >
+          {filtered.map(option => (
+            <div key={option.value} onClick={(_) => { toggleSelected(option); setDisplayOptions(false); }} className={`px-2 dark:hover:bg-zinc-800 ${isSelected(option) ? 'dark:bg-zinc-800' : ''}`}>{option.label}</div>
+          ))}
+        </div>
       </div>
-      <input type="text" id="multi-select-input" onFocus={() => setDisplayOptions(true)} onChange={(event) => filterResults(event.target.value)} />
-      <div className={`h-80 overflow-scroll ${displayOptions ? '' : 'hidden'}`}>
-        {filtered.map(option => (
-          <div key={option.value} onClick={(_) => { toggleSelected(option); setDisplayOptions(false); }} className={`dark:hover:bg-zinc-800 ${isSelected(option) ? 'dark:bg-zinc-800' : ''}`}>{option.label}</div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 }
