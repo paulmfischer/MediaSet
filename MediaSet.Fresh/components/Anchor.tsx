6ce3dce32f83e7d6915e5e81f3ad6e38ff3d30e7
Paul Fischer <me@paulmfischer.dev>
2024-08-25 11:31:06 -0500

    removing blazor/fresh frontends to reduce clutter

--- MediaSet.Fresh/components/Anchor.tsx
+++
@@ -1,9 +0,0 @@
-import { JSX } from "preact";
-
-export function Anchor({ children, className, ...props }: JSX.HTMLAttributes<HTMLAnchorElement>) {
-  return (
-    <a {...props} className={`text-blue-700 underline ${className}`}>
-      {children}
-    </a>
-  );
-}
