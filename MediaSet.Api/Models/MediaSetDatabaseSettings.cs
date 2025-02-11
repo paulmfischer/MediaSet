00932b245b37aa871da9acf6f8bffc6ddee29bbe
Paul Fischer <me@paulmfischer.dev>
2025-02-10 19:06:59 -0600

    remove books collection name (#154)

--- MediaSet.Api/Models/MediaSetDatabaseSettings.cs
+++ MediaSet.Api/Models/MediaSetDatabaseSettings.cs
@@ -4,5 +4,4 @@ public class MediaSetDatabaseSettings
 {
   public string ConnectionString { get; set; } = null!;
   public string DatabaseName { get; set; } = null!;
-  public string BooksCollectionName { get; set; } = null!;
 }
